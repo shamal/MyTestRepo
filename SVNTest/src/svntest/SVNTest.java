f905656e16e9c2470034d7c80e5ad9038bd27af9
Shamal Karunarathne <shamalk@gmail.com>
2012-04-23 02:25:57 -0700

    second update from native client

--- SVNTest/src/svntest/SVNTest.java
+++ SVNTest/src/svntest/SVNTest.java
@@ -14,6 +14,6 @@ public class SVNTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello SVN!");
     }
 }
